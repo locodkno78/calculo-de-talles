eb624695c9f02d375eaa2320253d4a43318e6c41
Santiago <dicolantonio@yahoo.com.ar>
2024-03-08 17:38:47 -0300

    identacion

--- talles/script.js
+++ talles/script.js
@@ -1,26 +1,24 @@
+var ancho = document.getElementById("ancho");
+var alto = document.getElementById("alto");
+var resultado = document.getElementById("talle");
+var lectura = document.getElementById("lectura");
+var calc = document.getElementById("calcular");
 
 
-    var ancho = document.getElementById("ancho");       
-    var alto = document.getElementById("alto"); 
-    var resultado = document.getElementById("talle");
-    var lectura = document.getElementById("lectura");
-    var calc = document.getElementById("calcular");
-       
+calc.onclick = function () {
+    if (ancho.value != "" && alto.value != "") {
+        talle = (Number(ancho.value) + Number(alto.value));
+        resultado.innerHTML = talle
+        console.log(talle);
 
-    calc.onclick = function(){
-        if(ancho.value!="" && alto.value!=""){
-            talle = (Number(ancho.value) + Number(alto.value));
-            resultado.innerHTML = talle
-            console.log(talle);
-    
-            if(talle<=20){ lectura.innerHTML = "Talle S"; }
-            else if(talle<=40){ lectura.innerHTML = "M"; }
-            else if(talle<=60){ lectura.innerHTML = "L"; }
-            else if(talle>30){ lectura.innerHTML = "XL"; }
-    
-        }else{
-            alert("Debes ingresar ancho y alto.")
-        }
-    
-    };
+        if (talle <= 20) { lectura.innerHTML = "Talle S"; }
+        else if (talle <= 40) { lectura.innerHTML = "M"; }
+        else if (talle <= 60) { lectura.innerHTML = "L"; }
+        else if (talle > 30) { lectura.innerHTML = "XL"; }
+
+    } else {
+        alert("Debes ingresar ancho y alto.")
+    }
+
+};
 
